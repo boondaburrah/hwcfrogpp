04f79e24393795e4480c410c4f2d18136279c45f
Luke A. Berry <berry.lu@ic10ddb1cee2ae.nunet.neu.edu>
2013-10-29 20:41:07 -0400

    Initial Commit

---
+++ hwcfrog/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  hwcfrog
+//
+//  Created by Luke A. Berry on 10/29/13.
+//  Copyright (c) 2013 Sputwiler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
